daacc2bb5f0d080347ec7bc116b2f81c376dd11e
의성 채 <iop245@naver.com>
2023-03-03 23:16:35 +0900

    feat: [003-email-verification] Customer Email Verification 추가

---
+++ src/main/java/com/zerobase/cms/user/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.zerobase.cms.user.exception;
+
+import com.zerobase.cms.user.domain.model.Customer;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode){
+        super(errorCode.getDetail());
+        this.errorCode = errorCode;
+    }
+}
--- src/main/java/com/zerobase/cms/user/service/SignUpCustomerService.java
+++ src/main/java/com/zerobase/cms/user/service/SignUpCustomerService.java
@@ -3,8 +3,14 @@ package com.zerobase.cms.user.service;
 import com.zerobase.cms.user.domain.SignUpForm;
 import com.zerobase.cms.user.domain.model.Customer;
 import com.zerobase.cms.user.domain.repository.CustomerRepository;
+import com.zerobase.cms.user.exception.CustomException;
+import com.zerobase.cms.user.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +21,29 @@ public class SignUpCustomerService {
         return customerRepository.save(Customer.from(form));
     }
 
+    public boolean isEmailExist(String email){
+        return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT)).isPresent();
+    }
 
+    public void verifyEmail(String email, String code){
+        Customer customer = customerRepository.findByEmail(email).orElseThrow(()->new CustomException(ErrorCode.USER_NOT_FOUND));
+        if(customer.isVerify()){
+            throw new CustomException(ErrorCode.ALREADY_VERIFY);
+        }
+        else if(!customer.getVerificationCode().equals(code)){
+            throw new CustomException(ErrorCode.VERIFICATION_CODE_UNMATCH);
+        }
+        else if(customer.getVerifyExpiredAt().isBefore(LocalDateTime.now())){
+            throw new CustomException(ErrorCode.EXPIRED_VERIFICATION_CODE);
+        }
+        customer.allowVerification();
+    }
+
+    @Transactional
+    public LocalDateTime changeCustomerValidation(Long customerId, String verificationCode){
+        Customer customer = customerRepository.findById(customerId).orElseThrow(()-> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        customer.issueVerificationCode(verificationCode);
+        return customer.getVerifyExpiredAt();
+    }
 }
