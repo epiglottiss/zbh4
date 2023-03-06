0f8c658b10e5155503327dd2bb8c7cb15cc98824
의성 채 <iop245@naver.com>
2023-03-06 20:55:07 +0900

    feat: [008-product-add] Product Add, Product Item 기능 추가
    Exception에 대한 response 추가
    
    Customer의 balance 기능 추가

--- order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
+++ order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
@@ -7,14 +7,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
-    ALREADY_REGISTER_ACCOUNT(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "없는 유저입니다."),
-    ALREADY_VERIFY(HttpStatus.BAD_REQUEST, "이미 등록된 계정입니다."),
-    VERIFICATION_CODE_UNMATCH(HttpStatus.BAD_REQUEST, "코드가 다릅니다."),
-    EXPIRED_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "만료된 코드 입니다."),
-    LOGIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "패스워드 오류"),
-
-    NOT_ENOUGH_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
+    NOT_FOUND_PRODUCT(HttpStatus.BAD_REQUEST, "상품이 없습니다."),
+    SAME_ITEM_NAME(HttpStatus.BAD_REQUEST, "중복된 아이템 입니다.")
     ;
 
     private final HttpStatus httpStatus;
